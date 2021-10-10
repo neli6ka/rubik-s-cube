b191226a846218581bcaba6bf96e7ac3edf21176
neli6ka <kamenova.nel@gmail.com>
2021-10-10 18:36:57 +0300

    Add view engine

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 
+app.set('views', path.resolve('./src/views'));
+app.engine('hbs', handlebars({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+
 app.all('/', (req, res) => {
-    res.write('It is working');
-    res.end();
+    res.render('index', { layout: false });
+    
 });
 
 app.listen(5000);
